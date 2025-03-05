8873525811fe00f70f8f90e09d6d1b3e0dcbb894
Saimon Islam Prantik <82695288+SaimonIslamPrantik@users.noreply.github.com>
2025-03-05 08:24:18 +0000

    Update main.ts
    Better-Unduck

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ function noSearchDefaultPageRender() {
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
-        <h1>Und*ck</h1>
+        <h1>Better-Und*ck</h1>
         <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
+        <p>Fork of <a href="https://github.com/t3dotgg/unduck">Und*ck</a> </p>
         <div class="url-container"> 
           <input 
             type="text" 
@@ -21,11 +22,11 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
+        <a href="Prantik" target="_blank">https://prantik1.netlify.app/</a>
         •
-        <a href="https://x.com/theo" target="_blank">theo</a>
+        <a href="https://x.com/saimonprantik" target="_blank">Twitter</a>
         •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/SaimonIslamPrantik/better-unduck/" target="_blank">github</a>
       </footer>
     </div>
   `;
